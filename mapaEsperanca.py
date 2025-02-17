cdb861e8535f62a6ea56df4a8574eb5e378e5b86
lrickelme <rickelmelucas200906@gmail.com>
2025-02-16 22:27:51 -0300

    Atualizando o mapa de Esperança

--- mapaEsperanca.py
+++ mapaEsperanca.py
@@ -1,25 +1,36 @@
 import requests
+from io import BytesIO
+from PIL import Image
 
-GEO_URL = "http://localhost:8080/geoserver/Lucas_Rickelme/wms"
-PARAMETROS = {
-    "service": "WMS",
-    "version": "1.1.1",
-    "request": "GetMap",
-    "layers": "Lucas_Rickelme:PB_Municipios_2023",
-    "styles": "",
-    "bbox": "-35.8597,-7.02278,-35.8497,-7.01278",
-    "width": "1920",
-    "height": "1080",
-    "srs": "EPSG:4674",
-    "format": "image/png",
-    "scale": "136000"
-}
+def request_wms_image(layer_name):
+    WMS_URL = "http://localhost:8080/geoserver/wms"
+    
+    params = {
+        "service": "WMS",
+        "version": "1.3.0",
+        "request": "GetMap",
+        "layers": f"{layer_name}:PB_Municipios_2023",
+        "styles": "",
+        "crs": "EPSG:4326",
+        "bbox": "-7.22,-36.06,-6.82,-35.66",
+        "width": "800",
+        "height": "600",
+        "format": "image/png",
+        "scale": "136000",
+        "transparent": "true"
+    }
+    
+    try:
+        response = requests.get(WMS_URL, params=params)
+        
+        if response.status_code == 200:
+            image = Image.open(BytesIO(response.content))
+            image.save("esperanca_wms.png", "PNG")
+            print("Imagem salva em: esperanca_wms.png")
+        else:
+            print(f"Erro na requisição: {response.status_code} - {response.reason}")
+    except Exception as e:
+        print("Erro ao requisitar a imagem do WMS:", e)
 
-response = requests.get(GEO_URL, params=PARAMETROS)
 
-if response.status_code == 200:
-    with open("PB_Municipios.png", "wb") as file:
-        file.write(response.content)
-    print("Mapa salvo como 'Esperanca.png'")
-else:
-    print(f"Erro ao obter a imagem WMS.")
+request_wms_image("Lucas_Rickelme")
--- mapaParaiba.py
+++ mapaParaiba.py
@@ -7,7 +7,7 @@ PARAMETROS = {
     "request": "GetMap",
     "layers": "Lucas_Rickelme:PB_Municipios_2023",
     "styles": "",
-    "bbox": "-38.7656034,-8.3029551,-34.7930865,-6.0259119",
+    
     "width": "1920",
     "height": "1080",
     "srs": "EPSG:4674",
@@ -23,3 +23,6 @@ if response.status_code == 200:
     print("Mapa salvo com o nome 'Municipios_da_Paraíba.png'")
 else:
     print(f"Erro ao obter a imagem WMS.")
+
+
+
