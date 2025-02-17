b37bf7877b3f477ef9ae7b66cc97576096a1abe8
lrickelme <rickelmelucas200906@gmail.com>
2025-02-16 22:34:39 -0300

    Atualizando geração do mapa da Paraíba

--- mapaParaiba.py
+++ mapaParaiba.py
@@ -1,28 +1,34 @@
 import requests
+from io import BytesIO
+from PIL import Image
 
-GEO_URL = "http://localhost:8080/geoserver/Lucas_Rickelme/wms"
-PARAMETROS = {
-    "service": "WMS",
-    "version": "1.1.1",
-    "request": "GetMap",
-    "layers": "Lucas_Rickelme:PB_Municipios_2023",
-    "styles": "",
+def request_wms_image():
+    GEO_URL = "http://localhost:8080/geoserver/Lucas_Rickelme/wms"
     
-    "width": "1920",
-    "height": "1080",
-    "srs": "EPSG:4674",
-    "format": "image/png",
-    "scale": "136000"
-}
+    params = {
+        "service": "WMS",
+        "version": "1.1.1",
+        "request": "GetMap",
+        "layers": "Lucas_Rickelme:PB_Municipios_2023",
+        "styles": "",
+        "bbox": "-38.7656034,-8.3029551,-34.7930865,-6.0259119",
+        "width": "1920",
+        "height": "1080",
+        "srs": "EPSG:4674",
+        "format": "image/png",
+        "scale": "136000"
+    }
+    
+    try:
+        response = requests.get(GEO_URL, params=params)
+        
+        if response.status_code == 200:
+            image = Image.open(BytesIO(response.content))
+            image.save("PB_Municipios.png", "PNG")
+            print("Mapa salvo com o nome 'PB_Municipios.png'")
+        else:
+            print(f"Erro na requisição: {response.status_code} - {response.reason}")
+    except Exception as e:
+        print("Erro ao requisitar a imagem do WMS:", e)
 
-response = requests.get(GEO_URL, params=PARAMETROS)
-
-if response.status_code == 200:
-    with open("PB_Municipios.png", "wb") as file:
-        file.write(response.content)
-    print("Mapa salvo com o nome 'Municipios_da_Paraíba.png'")
-else:
-    print(f"Erro ao obter a imagem WMS.")
-
-
-
+request_wms_image()
